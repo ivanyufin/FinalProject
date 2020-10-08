a83b3bd89a206e27c2d5108ed63955a83f2a1888
ivanyufin <61237211+ivanyufin@users.noreply.github.com>
2020-10-08 16:59:53 +0300

    createDatabase.sql

---
+++ createDatabase.sql
@@ -0,0 +1,39 @@
+﻿CREATE DATABASE Tutor
+GO
+
+USE Tutor
+
+CREATE TABLE [dbo].[table_DayLesson] (
+    [Id]         INT  IDENTITY (1, 1) NOT NULL,
+    [Day_Lesson] DATE NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+
+CREATE TABLE [dbo].[trial_lesson] (
+    [Id]    INT           IDENTITY (1, 1) NOT NULL,
+    [Name]  NVARCHAR (50) NOT NULL,
+    [Phone] NVARCHAR (50) NOT NULL,
+    [Email] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[Users] (
+    [Id]         INT           IDENTITY (1, 1) NOT NULL,
+    [LastName]   NVARCHAR (50) NOT NULL,
+    [FirstName]  NVARCHAR (50) NOT NULL,
+    [Patronymic] NVARCHAR (50) NOT NULL,
+    [Email]      NVARCHAR (50) NOT NULL,
+    [Phone]      NVARCHAR (50) NOT NULL,
+    [Password]   NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[Users_TimeLesson] (
+    [Id]          INT      IDENTITY (1, 1) NOT NULL,
+    [User_ID]     INT      NOT NULL,
+    [Time_Lesson] DATETIME NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
